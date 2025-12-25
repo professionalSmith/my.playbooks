4314529ee6497c623647ebc5e3f0a9195f00af1d
David Smith <3581358+professionalSmith@users.noreply.github.com>
2025-12-25 17:31:48 -0500

    fix(release): prevent git-hooks on bump
    Why
    ===
    
    Bump commits trigger pre-commit hooks which causes the commit to fail.
    
    This change addresses the need by
    =================================
    
    * adds commitzen flag to suppress git hooks when bumping versions

--- makefiles/release.mk
+++ makefiles/release.mk
@@ -2,23 +2,23 @@
 
 bump-check:
 	# Check what the next version is without making any changes
-	cz bump --yes --changelog --annotated-tag --dry-run --allow-no-commit
+	cz bump --yes --changelog --annotated-tag --no-verify --allow-no-commit --dry-run
 
 bump-major:
 	# Automatically bump the major version
-	cz bump --yes --changelog --annotated-tag --increment major
+	cz bump --yes --changelog --annotated-tag --no-verify --increment major
 
 bump-minor:
 	# Automatically bump the minor version
-	cz bump --yes --changelog --annotated-tag --increment minor
+	cz bump --yes --changelog --annotated-tag --no-verify --increment minor
 
 bump-patch:
 	# Automatically bump the patch version
-	cz bump --yes --changelog --annotated-tag --increment patch
+	cz bump --yes --changelog --annotated-tag --no-verify --increment patch
 
 bump:
 	# Automatically determine the next version and create a new tag
-	cz bump --yes --changelog --annotated-tag
+	cz bump --yes --changelog --annotated-tag --no-verify
 
 changelog:
 	# Generate or update the changelog
