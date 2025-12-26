fedff8619a8f33beb395c942a0f5f1d9de0706c2
David Smith <3581358+professionalSmith@users.noreply.github.com>
2025-12-25 21:32:10 -0500

    hotfix(releases): fix release tagging
    Why
    ===
    
    Command invocation doesn't work the same in Makefiles as it does from
    the command line. The $() encapsulation done at the command line
    apparently has to be assigned to a variable before use in Makefiles.
    
    This change addresses the need by
    =================================
    
    * assigned git tag query to variable for use in Make rules

--- makefiles/release.mk
+++ makefiles/release.mk
@@ -1,5 +1,7 @@
 .PHONY: all clean test
 
+TAG_LATEST := $(shell git describe --tags $(git rev-list --tags --max-count=1))
+
 bump-check:
 	# Check what the next version is without making any changes
 	cz bump --yes --changelog --annotated-tag --no-verify --allow-no-commit --dry-run
@@ -18,12 +20,11 @@ generate-changelog:
 
 push-tag:
 	# Push the newly created tag to the remote repository
-	git push origin $(git describe --tags $(git rev-list --tags --max-count=1))
+	git push origin $(TAG_LATEST)
 
 release: checkout-main-branch bump push-tag
 	# Perform a full release: bump version, update changelog, and push tag
-	gh release create $(git describe --tags $(git rev-list --tags --max-count=1)) \
-		--draft --generate-notes --latest --verify-tag
+	gh release create $(TAG_LATEST) --draft --generate-notes --latest --verify-tag
 
 validate-commits:
 	# Validate commit messages according to Conventional Commits
